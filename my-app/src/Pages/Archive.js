7599ac7afb6903a4092399a406264504b636c449
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2023-10-03 16:53:24 +0900

    html to react

--- my-app/src/Pages/Archive.js
+++ my-app/src/Pages/Archive.js
@@ -1,132 +1,103 @@
-import * as React from 'react';
-import '../style2.css'
+import * as React from "react";
+import "../style.css";
+import {Modal} from "./Modal";
+import "../style2.css";
 
-function Archive(){
-    return(
-        <div className='App'>
-            <div id="navbar">
-                <a id="logo" href="/">ABT</a>
-                <ul id="menu">
-                    <li id="contact"><a href="https://www.instagram.com/art.body.tech/">Contact</a></li>
-                    <li id="archive"><a href="./index2.html">Archive</a></li>
-                    <li id="info"><a class="btn js-click-modal">Info</a></li>
-                </ul>
-            </div>
+function Archive() {
+  const opencloseModal = () => {
+    if (document.querySelector(".container").classList.contains("modal-open")) {
+      document.querySelector(".container").classList.remove("modal-open");
+    } else {
+      document.querySelector(".container").classList.add("modal-open");
+    }
+  };
 
-            <div class="Copyright-2023-ABT">
-                Copyright @ 2023. ABT
-            </div>
+  return (
+    <div className="App">
+      <div id="navbar">
+        <a id="logo" href="/">
+          ABT
+        </a>
+        <ul id="menu">
+          <li id="contact">
+            <a href="https://www.instagram.com/art.body.tech/">Contact</a>
+          </li>
+          <li id="archive">
+            <a href="/archive">Archive</a>
+          </li>
+          <li id="info">
+            <a
+              className="btn js-click-modal"
+              onClick={() => {
+                opencloseModal();
+              }}
+            >
+              Info
+            </a>
+          </li>
+        </ul>
+     </div>
 
-            <div class="neuron">
-                <img src="static/neuron.png"/>
-                <span class="digital-performance-study">
-                    digital performance study
-                </span>
-                <span class="dps-1">
-                    <a href="dps-1.html">1</a>
-                </span>
-                <span class="dps-2">
-                    <a href="dps-2.html">2</a>
-                </span>
-                <span class="dps-3">
-                    <a class="btn js-click-modal2">3</a>
-                </span>
-                <span class="meeting-with-plannerartists">
-                    meeting with planner/artists
-                </span>
-                <span class="mwp-1">
-                    <a class="btn js-click-modal2">1</a>
-                </span>
-                <span class="mwp-2">
-                    <a class="btn js-click-modal2">2</a>
-                </span>
-                <span class="mwp-3">
-                    <a class="btn js-click-modal2">3</a>
-                </span>
-                <span class="hongyeon-gil-dataset">
-                    hongyeon-gil dataset
-                </span>
-                <span class="hgd-1">
-                    <a class="btn js-click-modal2">1</a>
-                </span>
-                <span class="hgd-2">
-                    <a class="btn js-click-modal2">2</a>
-                </span>
-                <span class="hgd-3">
-                    <a class="btn js-click-modal2">3</a>
-                </span>
-                <span class="developing-moments">
-                    developing moments
-                </span>
-                <span class="dm-1">
-                    <a class="btn js-click-modal2">1</a>
-                </span>
-                <span class="dm-2">
-                    <a class="btn js-click-modal2">2</a>
-                </span>
-                <span class="dm-3">
-                    <a class="btn js-click-modal2">3</a>
-                </span>
-                <span class="GO-TO-MUSEUM">
-                    GO TO MUSEUM!
-                </span>
-                <span class="gm-1">
-                    <a class="btn js-click-modal2">1</a>
-                </span>
-                <span class="gm-2">
-                    <a class="btn js-click-modal2">2</a>
-                </span>
-                <span class="gm-3">
-                    <a class="btn js-click-modal2">3</a>
-                </span>
-            </div>
+    <div className="Copyright-2023-ABT">Copyright @ 2023. ABT</div>
 
-            <div class="container">
-                <div class="modal">
-                    <div class="row">
-                        <div class="column">
-                            <h2 class="title">아바타 ABT</h2>
-                            <div class="text1">
-                                <span>팀명 아바타(ABT)는 영화 아바타 Avatar의 새로운 생명체 '아바타'에서 따와 팀의 키워드인 아트(Art), 바디(Body), 테크(Tech)의 약자로 재해석해 탄생했다. 수행과 매체를 연구하는 기획자 최은총, 퍼포먼스 작가 임은정, 아트앤테크 작가 장예원, 미디어 작가 도재인은 서로의 전문성을 융합한 '디지털 퍼포먼스'를 선보인다. ‘디지털 퍼포먼스’는 매체-신체 키워드에 해당하는 한 주제로, 미디어와 테크놀로지 그리고 퍼포먼스가 결합된 장르를 통칭한다.
-                                    아바타는 2023년 8월부터 메타-홍연길 프로젝트(Meta-Hongyeon Road Project)를 선보이며 사회-문화-자연을 연결하는 융복합 프로젝트를 선보일 예정이다. 프로젝트는 AI/웹/설치/영상/퍼포먼스 등 다양한 장르의 현대예술을 온/오프라인을 통해 선보일 예정이다. 더하여 국/내외 합동 교류 프로젝트 기획하여 지역과 세계를 연결하는 국제적인 네트워킹 기회를 만들고자 한다.
-                                    <br/><br/>
-                                    멤버 Members<br/>
-                                    도재인/Tao Jane<br/>
-                                    임은정/Im Eunjoung<br/>
-                                    장예원/Yewon Jang <br/>
-                                    최은총/Choi Eunchong</span>
-                            </div>
-                        </div>
-                        <div class="column">
-                            <h2 class="title">아바타 ABT</h2>
-                            <div class="text2">
-                                <span>The team name 아바타(ABT) is derived from the new life form "Avatar" in the movie Avatar and reinterpreted as an acronym for the team's keywords Art, Body, and Tech. Choi Eunchong, a curator who researches performance and media, a performance artist Im Eunjoung, a interactive artist Jang Yewon, and a media artist Tao Jane, combines their expertise and will present "Digital Performance." "Digital Performance" is a genre that corresponds to the media-body keywords, combining media, technology, and performance.
-
-                                    Starting in August 2023, 아바타 will present the Meta-Hong-yeon-Gil Project, planning to introduce a convergent project that connects society, culture, and nature. The project will showcase various genres of contemporary art, including AI, web, installation, video, and performance, on online platforms and offline places. In addition, joint domestic and international exchange projects will be organized to create international networking opportunities that connect the region and the world.</span>
-                            </div>
-                        </div>
-                        <div class="column">
-                            <h2 class="title">아이덴티티 디자인</h2>
-                            <img class="logo-image" src="static/logo_small.png" />
-                            <div class="text1">
-                                <p>‘ 아바타의 알파벳 ABT로, 몸으로 자유롭게 감각하는 형태를 본떠서 유동적인 로고타입을 제작하였다. A는 걸어가는 다리의 형상과 한자 사람인(人)의 의미를 담았으며, B는 몸의 형태, 그리고 T는 만세 하는 듯한 형상을 추상적으로 나타내었다. 알파벳 주위에 있는 네명의 사람 또한 네명의 팀원들을 의미한다. 반투명한 파랑색 물의 재질과 미세한 초록색 금속 질감은, 유동적인 몸의 움직임과 기술이 공존하는 아바타 팀의 특성을 드러낸다. <br/>(디자인/글, 도재인)
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="container2">
-                <div class="modal2">
-                    <div class="alarm">
-                        진행 중 ...
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+      <div className="neuron">
+        <img src="static/neuron.png" />
+        <span className="digital-performance-study">digital performance study</span>
+        <span className="dps-1">
+          <a href="/dps">1</a>
+        </span>
+        <span className="dps-2">
+          <a href="dps-2.html">2</a>
+        </span>
+        <span className="dps-3">
+          <a className="btn js-click-modal2">3</a>
+        </span>
+        <span className="meeting-with-plannerartists">
+          meeting with planner/artists
+        </span>
+        <span className="mwp-1">
+          <a className="btn js-click-modal2">1</a>
+        </span>
+        <span className="mwp-2">
+          <a className="btn js-click-modal2">2</a>
+        </span>
+        <span className="mwp-3">
+          <a className="btn js-click-modal2">3</a>
+        </span>
+        <span className="hongyeon-gil-dataset">hongyeon-gil dataset</span>
+        <span className="hgd-1">
+          <a className="btn js-click-modal2">1</a>
+        </span>
+        <span className="hgd-2">
+          <a className="btn js-click-modal2">2</a>
+        </span>
+        <span className="hgd-3">
+          <a className="btn js-click-modal2">3</a>
+        </span>
+        <span className="developing-moments">developing moments</span>
+        <span className="dm-1">
+          <a className="btn js-click-modal2">1</a>
+        </span>
+        <span className="dm-2">
+          <a className="btn js-click-modal2">2</a>
+        </span>
+        <span className="dm-3">
+          <a className="btn js-click-modal2">3</a>
+        </span>
+        <span className="GO-TO-MUSEUM">GO TO MUSEUM!</span>
+        <span className="gm-1">
+          <a className="btn js-click-modal2">1</a>
+        </span>
+        <span className="gm-2">
+          <a className="btn js-click-modal2">2</a>
+        </span>
+        <span className="gm-3">
+          <a className="btn js-click-modal2">3</a>
+        </span>
+      </div>
+      <Modal />
+    </div>
+  );
 }
 
 export default Archive;
