43d022010ada3f576efc9e4c07f912add1f69381
designerSejinOH <ohsj1999@naver.com>
2024-01-13 21:48:20 +0900

    Add parallax

--- my-app/src/pages/works.jsx
+++ my-app/src/pages/works.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useRef } from 'react'
-import { SectionScrollPage } from './works/SectionScroll'
+import { SectionScrollPage } from '../components/dom/works/SectionScroll'
 import { ParallaxProvider } from 'react-scroll-parallax'
 import Scene from '@/components/canvas/Scene'
 import { Canvas } from '@/components/canvas/Canvas'
